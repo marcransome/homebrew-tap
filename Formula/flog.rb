6ce02b80d4b9c89e0ba1e6869e552351cc236711
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2023-01-16 17:16:46 +0000

    Update flog SHA-256 checksum

--- Formula/flog.rb
+++ Formula/flog.rb
@@ -2,7 +2,7 @@ class Flog < Formula
   desc "Command-line tool for sending log messages to Apple's unified logging system"
   homepage "https://github.com/marcransome/flog"
   url "https://github.com/marcransome/flog/archive/1.3.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "b62df72ca6a97c54c3fc396e5f028db257b9c09b5c9aaa5fd8031d2ee586d112"
   license "MIT"
 
   depends_on "cmake" => :build
