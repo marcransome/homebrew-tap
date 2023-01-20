c11c9f60db471d29d2af4fe01ac145224621b296
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2023-01-20 23:03:38 +0000

    Fix file extension

--- Casks/plist-services.rb
+++ Casks/plist-services.rb
@@ -2,7 +2,7 @@ cask "plist-services" do
   version "1.0.0"
   sha256 "b6d23a413154eba7927dd184daa37c9cd62e1d25147bf53b47db7c5418b57fb3"
 
-  url "https://github.com/marcransome/plist-services/archive/#{version}.tar.gz.zip"
+  url "https://github.com/marcransome/plist-services/archive/#{version}.tar.gz"
   name "plist-services"
   desc "macOS Quick Actions for converting property list files between XML and binary encodings"
   homepage "https://www.github.com/marcransome/plist-services"
