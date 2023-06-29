89dc9034cce4d55b37f1ccd0d286551869427b85
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2023-06-29 23:00:23 +0100

    Bump flog version and add popt dependency

--- Formula/flog.rb
+++ Formula/flog.rb
@@ -1,11 +1,12 @@
 class Flog < Formula
   desc "Command-line tool for sending log messages to Apple's unified logging system"
   homepage "https://github.com/marcransome/flog"
-  url "https://github.com/marcransome/flog/archive/1.3.0.tar.gz"
-  sha256 "b62df72ca6a97c54c3fc396e5f028db257b9c09b5c9aaa5fd8031d2ee586d112"
+  url "https://github.com/marcransome/flog/archive/1.4.0.tar.gz"
+  sha256 "7fba0b9ff68b6c283d30f53f0d221750a11d84e7cf744662aedf3973b1744677"
   license "MIT"
 
   depends_on "cmake" => :build
+  depends_on "popt"
   depends_on :macos
 
   def install
