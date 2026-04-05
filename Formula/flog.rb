0253d2a8983eab77dd6306f47c0b0fb4a0593e19
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2026-04-05 15:57:47 +0100

    Bump flog version

--- Formula/flog.rb
+++ Formula/flog.rb
@@ -1,8 +1,8 @@
 class Flog < Formula
   desc "Command-line tool for sending log messages to Apple's unified logging system"
   homepage "https://github.com/marcransome/flog"
-  url "https://github.com/marcransome/flog/archive/refs/tags/v1.7.3.tar.gz"
-  sha256 "41c251ecdea78927b9284b6a64e456ebd5468c008ee5c0e9962f9e5c1c469110"
+  url "https://github.com/marcransome/flog/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "d6274c987cefbff14a470e1b642b76839faff7c7993d7a7dc0700b75c27f2db5"
   license "MIT"
 
   depends_on :macos
@@ -10,13 +10,13 @@ class Flog < Formula
 
   resource "binary" do
     on_arm do
-      url "https://github.com/marcransome/flog/releases/download/v1.7.3/flog-v1.7.3-darwin-arm64.tar.xz"
-      sha256 "95c1020f1f786cae7080adcabeeb1870ec71035c1ab6c9c221a0d0c305a2b96b"
+      url "https://github.com/marcransome/flog/releases/download/v1.8.0/flog-v1.8.0-darwin-arm64.tar.xz"
+      sha256 "48068971a64dd5ab7a617dba293c7ee64ef51eb92b0b49ef2e22ef62984eed61"
     end
 
     on_intel do
-      url "https://github.com/marcransome/flog/releases/download/v1.7.3/flog-v1.7.3-darwin-x86_64.tar.xz"
-      sha256 "022c417d193567ce7795579624f8b3eb9dad7ebb46f6f99b57523f7073aa8f0c"
+      url "https://github.com/marcransome/flog/releases/download/v1.8.0/flog-v1.8.0-darwin-x86_64.tar.xz"
+      sha256 "0a4dfca9d5668824b9dd50b9c2881a0c10ddbe91ec7cada82180a4381fe0609c"
     end
   end
 
@@ -32,14 +32,14 @@ class Flog < Formula
     assert_predicate bin/"flog", :executable?
     assert_path_exists prefix/"README.md"
     assert_path_exists prefix/"LICENSE"
-    assert_equal "flog v1.7.3", shell_output("#{bin}/flog --version").chomp
+    assert_equal "flog v1.8.0", shell_output("#{bin}/flog --version").chomp
     system bin/"flog",
         "--subsystem", "uk.co.fidgetbox.flog",
         "--category", "internal",
         "--level", "debug",
         "--append", testpath/"output.txt",
-        "Flog v1.7.3 test message"
+        "Flog v1.8.0 test message"
     assert_path_exists testpath/"output.txt"
-    assert_match "Flog v1.7.3 test message", shell_output("cat #{testpath}/output.txt").chomp
+    assert_match "Flog v1.8.0 test message", shell_output("cat #{testpath}/output.txt").chomp
   end
 end
